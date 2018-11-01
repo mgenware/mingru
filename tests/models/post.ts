f81d2a355e00ca7a7452c058a046d458baa56bc0
Mgen <m@mgenware.com>
2018-11-02 01:48:55 +0800

    Add more tests for select

--- tests/models/post.ts
+++ tests/models/post.ts
@@ -5,6 +5,7 @@ class Post extends dd.Table {
   id = dd.pk();
   title = dd.varChar(100);
   user_id = user.id;
+  reviewer_id = user.id;
 }
 
 export default dd.table(Post);
--- tests/select.test.ts
+++ tests/select.test.ts
@@ -14,3 +14,12 @@ test('Basic join', () => {
   const v = new Select('t', [post.user_id.join(user).nick, post.title], post);
   expect(v.convert(mysql).sql).toBe('SELECT `_join_1`.`nick`, _main.`title` FROM `post` AS _main INNER JOIN `user` AS `_join_1` ON `_join_1`.`id` = `_main`.`user_id`');
 });
+
+test('Same table, multiple cols join', () => {
+  const v = new Select('t', [
+    post.user_id.join(user).nick,
+    post.user_id.join(user).id,
+    post.reviewer_id.join(user).nick,
+  ], post);
+  expect(v.convert(mysql).sql).toBe('SELECT `_join_1`.`nick`, `_join_1`.`id`, `_join_2`.`nick` FROM `post` AS _main INNER JOIN `user` AS `_join_1` ON `_join_1`.`id` = `_main`.`user_id` INNER JOIN `user` AS `_join_2` ON `_join_2`.`id` = `_main`.`reviewer_id`');
+});
