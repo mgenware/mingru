0c9687f7fc1f39f8350670cfb36b35883732616b
Mgen <m@mgenware.com>
2018-11-18 19:27:18 +0800

    Add tests for goType

--- lib/dialects/mysql.ts
+++ lib/dialects/mysql.ts
@@ -1,7 +1,40 @@
 import Dialect from '../dialect';
+import * as dd from 'dd-models';
+import { throwIfFalsy } from 'throw-if-arg-empty';
+import { inspect } from 'util';
 
 export default class MySQL extends Dialect {
   escape(name: string): string {
+    throwIfFalsy(name, 'name');
     return '`' + name + '`';
+  }
+
+  goType(column: dd.Column): string {
+    throwIfFalsy(column, 'column');
+
+    const DT = dd.dt;
+    const unsigned = column.unsigned;
+    for (const type of column.types) {
+      switch (type) {
+        case DT.bigInt: {
+          return unsigned ? 'uint64' : 'int64';
+        }
+        case DT.int: {
+          return unsigned ? 'uint' : 'int';
+        }
+        case DT.smallInt: {
+          return unsigned ? 'uint16' : 'int16';
+        }
+        case DT.tinyInt: {
+          return unsigned ? 'uint8' : 'int8';
+        }
+        case DT.varChar:
+        case DT.char:
+        case DT.text: {
+          return 'string';
+        }
+      }
+    }
+    throw new Error(`Type not supported: ${inspect(column.types)}`);
   }
 }
--- tests/dialects/mysql.test.ts
+++ tests/dialects/mysql.test.ts
@@ -1,7 +1,27 @@
 import * as mr from '../..';
+import * as dd from 'dd-models';
 
 const dialect = new mr.MySQL();
 
 test('escape', () => {
   expect(dialect.escape('abc')).toBe('`abc`');
+});
+
+test('DT', () => {
+  const tests: Array<Array<unknown>> = [
+    [dd.int(), 'int'],
+    [dd.unsignedInt(), 'uint'],
+    [dd.bigInt(), 'int64'],
+    [dd.unsignedBigInt(), 'uint64'],
+    [dd.smallInt(), 'int16'],
+    [dd.unsignedSmallInt(), 'uint16'],
+    [dd.tinyInt(), 'int8'],
+    [dd.unsignedTinyInt(), 'uint8'],
+  ];
+
+  for (const t of tests) {
+    const obj = t[0] as dd.Column;
+    const e = t[1] as string;
+    expect(dialect.goType(obj)).toBe(e);
+  }
 });
