0df930e0145626aeb755a7771dc713b6b7dc5ad1
Mgen <m@mgenware.com>
2018-11-21 01:24:01 +0800

    [WIP] Update goBuilder

--- src/builder/goBuilder.ts
+++ src/builder/goBuilder.ts
@@ -3,29 +3,60 @@ import { capitalizeFirstLetter } from '../lib/stringUtil';
 import Dialect from '../dialect';
 import * as dd from 'dd-models';
 import { throwIfFalsy } from 'throw-if-arg-empty';
+import { Action } from 'dd-models/dist/actions/action';
+import { inspect } from 'util';
 
 const INDENT = '\t';
 const NEWLINE = '\n';
+const QUERYABLE = 'queryable sqlx.Queryable';
 
 export default class GoBuilder {
   memberNames: Set<string> = new Set<string>();
 
+  private tableName: string;
+  private daType: string;
+
   constructor(
     public dialect: Dialect,
-  ) { }
-
-  build(tableActions: dd.TableActionCollection): string {
+    public tableActions: dd.TableActionCollection,
+  ) {
     throwIfFalsy(tableActions, 'tableActions');
-    return '';
+    this.tableName = this.tableActions.table.__name;
+    this.daType = `${this.tableName}DA`;
   }
 
-  select(io: SelectIO): string {
-    const { dialect } = this;
+  build(): string {
+    const { tableActions } = this;
+
+    let code = this.buildDataObject(tableActions);
+    code += `// ----- Actions ----- //${NEWLINE}`;
+    for (const [_, action] of tableActions.map) {
+      code += this.buildAction(action);
+    }
+    return code;
+  }
+
+  private buildDataObject(tableActions: dd.TableActionCollection): string {
+    const tableName = tableActions.table.__name;
+    const clsName = tableName + 'TableType';
+    const objName = capitalizeFirstLetter(tableName);
+    let code = `type ${clsName} struct {${NEWLINE}}${NEWLINE}`;
+    code += `var ${objName} = &${clsName}{}${NEWLINE}${NEWLINE}`;
+    return code;
+  }
+
+  private buildAction(io: unknown): string {
+    if (io instanceof SelectIO) {
+      return this.select(io as SelectIO);
+    }
+    throw new Error(`Not supported io object "${inspect(io)}"`);
+  )
+
+  private select(io: SelectIO): string {
+    const { dialect, daType } = this;
     const actionName = capitalizeFirstLetter(io.action.name);
     const funcName = this.getMemberName('Select', actionName);
-    this.checkMemberName(funcName);
     const clsName = funcName + 'Result';
-    this.checkMemberName(clsName);
 
     let code = '';
     // Build result type
@@ -45,18 +76,21 @@ export default class GoBuilder {
     }
     code += `}${NEWLINE}${NEWLINE}`;
 
+    // Build func
+    let paramsCode = QUERYABLE;
+    if (io.where) {
+      for (const element of io.where.sql.elements) {
+        if (element instanceof dd.InputParam) {
+          const input = element as dd.InputParam;
+        }
+      }
+    }
+    code += `func (da *${daType}) ${actionName}()`;
     return code;
   }
 
   private getMemberName(prefix: string, viewName: string) {
     const name = prefix + viewName;
     return name;
-  }
-
-  private checkMemberName(name: string) {
-    if (this.memberNames.has(name)) {
-      throw new Error(`The member name "${name}" already exists`);
-    }
-    this.memberNames.add(name);
   }
 }
