d852f355aef47f2d03aaf80bad6df5a0487fad13
Mgen <m@mgenware.com>
2018-11-25 15:49:40 +0800

    Add tests for 3-table joins

--- tests/io/select.test.ts
+++ tests/io/select.test.ts
@@ -2,6 +2,7 @@ import * as mr from '../../';
 import * as dd from 'dd-models';
 import user from '../models/user';
 import post from '../models/post';
+import cmt from '../models/cmt';
 
 const dialect = new mr.MySQL();
 
@@ -54,4 +55,18 @@ test('Same table, multiple cols join', () => {
   const io = mr.select(v, dialect);
 
   expect(io.sql).toBe('SELECT `_join_1`.`name`, `_join_1`.`id`, `_join_2`.`name` FROM `post` AS `_main` INNER JOIN `user` AS `_join_1` ON `_join_1`.`id` = `_main`.`user_id` INNER JOIN `user` AS `_join_2` ON `_join_2`.`id` = `_main`.`reviewer_id`');
+});
+
+test('3-table joins', () => {
+  const actions = dd.actions(cmt);
+  const v = actions.select(
+      't',
+      cmt.id,
+      cmt.user_id,
+      cmt.target_id.join(post).user_id,
+      cmt.target_id.join(post).title,
+    );
+  const io = mr.select(v, dialect);
+
+  expect(io.sql).toBe('SELECT `_main`.`id`, `_main`.`user_id`, `_join_1`.`user_id`, `_join_1`.`title` FROM `post_cmt` AS `_main` INNER JOIN `post` AS `_join_1` ON `_join_1`.`id` = `_main`.`target_id`');
 });
