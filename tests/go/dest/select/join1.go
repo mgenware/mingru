c5268f5bd2242cb07984ddb18b7ddbda7dd6d5af
Mgen <m@mgenware.com>
2018-11-27 01:40:25 +0800

    Fix failed go join SQL tests

--- tests/go/dest/select/join1.go
+++ tests/go/dest/select/join1.go
@@ -10,7 +10,7 @@ type SelectTResult struct {
 // SelectT ...
 func (da *TableTypePostCmtRpl) SelectT(queryable sqlx.Queryable) (*SelectTResult, error) {
 	result := &SelectTResult{}
-	err := queryable.QueryRow("SELECT `_join_1`.`url_name`AS `postCmtRplUserUrlName`, `_join_1`.`id` AS `postCmtRplUserID`, `_join_2`.`url_name` AS `postCmtRplToUserUrlName` FROM `post_cmt_rpl` AS `_main` INNER JOIN `user` AS `_join_1` ON `_join_1`.`id` = `_main`.`user_id` INNER JOIN `user` AS `_join_2` ON `_join_2`.`id` = `_main`.`to_user_id`").Scan(&result.PostCmtRplUserUrlName, &result.PostCmtRplUserID, &result.PostCmtRplToUserUrlName)
+	err := queryable.QueryRow("SELECT `_join_1`.`url_name` AS `postCmtRplUserUrlName`, `_join_1`.`id` AS `postCmtRplUserID`, `_join_2`.`url_name` AS `postCmtRplToUserUrlName` FROM `post_cmt_rpl` AS `_main` INNER JOIN `user` AS `_join_1` ON `_join_1`.`id` = `_main`.`user_id` INNER JOIN `user` AS `_join_2` ON `_join_2`.`id` = `_main`.`to_user_id`").Scan(&result.PostCmtRplUserUrlName, &result.PostCmtRplUserID, &result.PostCmtRplToUserUrlName)
 	if err != nil {
 		return nil, err
 	}
