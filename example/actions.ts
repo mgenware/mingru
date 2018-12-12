7db6dcd703ba730e08e8029b97b1f1a36acc1f42
Mgen <m@mgenware.com>
2018-12-12 20:43:23 +0800

    Update example

--- example/actions.ts
+++ example/actions.ts
@@ -4,28 +4,25 @@ import post from './models/post';
 
 const userTA = dd.actions(user);
 // Select a single row by ID
-userTA
-  .select('Profile', user.display_name, user.sig)
-  .where(user.id.toInputSQL());
+userTA.select('UserProfile', user.display_name, user.sig).byID();
+// Select all rows
+userTA.selectAll('AllUserProfiles', user.display_name, user.sig);
+// Select a single field of a row
+userTA.selectField('UserName', user.display_name).byID();
 // Update a row
 userTA
-  .update('Profile')
+  .update('UserProfile')
   .setInputs(user.display_name, user.sig)
-  .where(user.id.toInputSQL());
+  .byID();
 // Delete a row by ID
 userTA.deleteOne('ByID').byID();
 
 const postTA = dd.actions(post);
-postTA.select(
-  'PostInfo',
-  post.id,
-  post.content,
-  post.user_id.join(user).url_name,
-);
+postTA.select('Post', post.id, post.content, post.user_id.join(user).url_name);
 postTA
   .update('Content')
   .setInputs(post.content)
-  .where(post.id.toInputSQL());
+  .byID();
 postTA.deleteOne('ByID').byID();
 
 export default [userTA, postTA];
