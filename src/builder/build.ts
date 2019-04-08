040507c78775ece80c6022ce1044e06ec6dab764
Mgen <m@mgenware.com>
2019-04-08 20:25:08 +0800

    Add cleanBuild option

--- src/builder/build.ts
+++ src/builder/build.ts
@@ -4,10 +4,12 @@ import { throwIfFalsy } from 'throw-if-arg-empty';
 import * as mfs from 'm-fs';
 import Dialect from '../dialect';
 import * as nodepath from 'path';
+import del from 'del';
 
 export interface IBuildOption {
   packageName?: string;
   noFileHeader?: boolean;
+  cleanBuild?: boolean;
 }
 
 export default async function buildAsync(
@@ -21,8 +23,16 @@ export default async function buildAsync(
   throwIfFalsy(outDir, 'outDir');
   const opts = options || {};
 
+  if (opts.cleanBuild) {
+    // tslint:disable-next-line no-console
+    console.log(`🧹  Cleaning directory "${outDir}"`);
+    await del(outDir, { force: true });
+  }
+
   await Promise.all(
     tableActionList.map(async action => {
+      // tslint:disable-next-line no-console
+      console.log(`🚙  Building table "${action.table.__name}"`);
       const builder = new GoBuilder(action, dialect, opts.packageName);
       const code = builder.build(false, !!opts.noFileHeader);
       const fileName = dd.utils.toSnakeCase(action.table.__name) + '_ta'; // Add a "_ta" suffix to table actions file
@@ -30,4 +40,6 @@ export default async function buildAsync(
       await mfs.writeFileAsync(outFile, code, 'utf8');
     }),
   );
+  // tslint:disable-next-line no-console
+  console.log(`🎉  Build succeeded`);
 }
