08367713e6c04857a3bcbc8d6b1a7a1fe4824d86
Mgen <m@mgenware.com>
2019-06-14 19:07:24 +0800

    Add tests for WrapIO.innerIO

--- tests/io/wrap.test.ts
+++ tests/io/wrap.test.ts
@@ -8,8 +8,7 @@ class WrapSelfTA extends dd.TA {
   s = dd
     .updateSome()
     .set(user.url_name, dd.sql`${dd.input(user.url_name)}`)
-    .setInputs(user.sig)
-    .set(user.follower_count, dd.sql`${user.follower_count} + 1`)
+    .setInputs(user.sig, user.follower_count)
     .where(
       dd.sql`${user.url_name.toInput()} ${user.id.toInput()} ${user.url_name.toInput()}`,
     );
@@ -22,12 +21,18 @@ test('WrapIO', () => {
   expect(io).toBeInstanceOf(mr.WrapIO);
 });
 
-test('getInputs', () => {
+test('getInputs (wrapSelf and innerIO)', () => {
   const io = mr.wrapIO(wrapSelf.d, dialect);
   expect(io.funcArgs.toString()).toEqual(
-    'urlName: string, id: uint64, urlName: string {urlName: string, id: uint64}',
+    'urlName: string, id: uint64, urlName: string, followerCount: *string {urlName: string, id: uint64, followerCount: *string}',
+  );
+  expect(io.innerIO.funcArgs.toString()).toEqual(
+    'urlName: string, id: uint64, urlName: string, sig: *string, followerCount: *string {urlName: string, id: uint64, sig: *string, followerCount: *string}',
   );
   expect(io.execArgs.toString()).toEqual(
-    'urlName: string, sig: *string="haha", urlName: string, id: uint64, urlName: string {urlName: string, sig: *string="haha", id: uint64}',
+    'urlName: string, sig: *string="haha", followerCount: *string, urlName: string, id: uint64, urlName: string {urlName: string, sig: *string="haha", followerCount: *string, id: uint64}',
+  );
+  expect(io.innerIO.execArgs.toString()).toEqual(
+    'urlName: string, sig: *string, followerCount: *string, urlName: string, id: uint64, urlName: string {urlName: string, sig: *string, followerCount: *string, id: uint64}',
   );
 });
