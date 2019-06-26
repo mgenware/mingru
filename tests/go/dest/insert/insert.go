e190bf73f6bd4506bd672ca091cba29c638bc1a3
Mgen <m@mgenware.com>
2019-06-27 01:42:40 +0800

    Fix failed tests

--- tests/go/dest/insert/insert.go
+++ tests/go/dest/insert/insert.go
@@ -16,7 +16,7 @@ var Post = &TableTypePost{}
 // ------------ Actions ------------
 
 // InsertT ...
-func (da *TableTypePost) InsertT(queryable dbx.Queryable, title string, userID uint64, content string, cmtCount uint, datetime time.Time, date time.Time, time time.Time, nDatetime *time.Time, nDate *time.Time, nTime *time.Time) error {
-	_, err := queryable.Exec("INSERT INTO `post` (`title`, `user_id`, `content`, `cmt_c`, `datetime`, `date`, `time`, `n_datetime`, `n_date`, `n_time`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", title, userID, content, cmtCount, datetime, date, time, nDatetime, nDate, nTime)
+func (da *TableTypePost) InsertT(queryable dbx.Queryable, title string, userID uint64, content string, reviewerID uint64, cmtCount uint, datetime time.Time, date time.Time, time time.Time, nDatetime *time.Time, nDate *time.Time, nTime *time.Time, mUserID uint64) error {
+	_, err := queryable.Exec("INSERT INTO `post` (`title`, `user_id`, `content`, `reviewer_id`, `cmt_c`, `datetime`, `date`, `time`, `n_datetime`, `n_date`, `n_time`, `my_user_id`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", title, userID, content, reviewerID, cmtCount, datetime, date, time, nDatetime, nDate, nTime, mUserID)
 	return err
 }
