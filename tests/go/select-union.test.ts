eaa52f785b08efd1513f1473a7ba5a23f50882a1
Mgen <m@mgenware.com>
2020-11-15 20:51:33 +0800

    Add tests for UNION starting from another member

--- tests/go/select-union.test.ts
+++ tests/go/select-union.test.ts
@@ -24,3 +24,26 @@ it('UNION', async () => {
 
   await testBuildAsync(ta, 'select-union/union');
 });
+
+it('UNION starting from another member', async () => {
+  class Activity extends mm.GhostTable {}
+  const activity = mm.table(Activity);
+  class ActivityTA extends mm.TableActions {
+    #privateT = mm
+      .select(user.id, user.sig.as('generic_sig'), user.url_name.as('generic_name'))
+      .from(user)
+      .by(user.id)
+      .privateAttr();
+
+    t = this.#privateT.union(
+      mm
+        .select(post.id, post.title)
+        .from(post)
+        .by(post.id, 'postID')
+        .unionAll(mm.selectRows(like.user_id, like.value).from(like)),
+    );
+  }
+  const ta = mm.tableActions(activity, ActivityTA);
+
+  await testBuildAsync(ta, 'select-union/unionAnotherMember');
+});
