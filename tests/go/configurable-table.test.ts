f82da5bbae8a8063fd944544ecbdfe0d649f05b2
Mgen <m@mgenware.com>
2022-03-07 23:38:10 +0800

    Add more action tests for configurable tables

--- tests/go/configurable-table.test.ts
+++ tests/go/configurable-table.test.ts
@@ -18,17 +18,50 @@ it('configurableTable', async () => {
 it('configurableTable with WRAP action', async () => {
   // Two tables with the almost same structure.
   class UserUtil extends User {}
-  const userUtil = mm.table(UserUtil);
+  const userUtil = mm.table(UserUtil, { virtualTable: true });
   class CommonTA extends mm.TableActions {
     insert = mm.insertOne().setInputs();
+    del = mm.deleteOne().by(userUtil.id);
+    upd = mm.updateOne().setInputs().by(userUtil.id);
+    sel = mm.selectRows(userUtil.display_name).by(userUtil.id).orderByAsc(userUtil.display_name);
   }
   const commonTA = mm.tableActions(userUtil, CommonTA, { configurableTableName: 'mrFromTable' });
 
   class ConsumerTA extends mm.TableActions {
     addUser = commonTA.insert.wrap({ mrFromTable: user });
     addPost = commonTA.insert.wrap({ mrFromTable: post });
+    delPost = commonTA.del.wrap({ mrFromTable: post });
+    updPost = commonTA.upd.wrap({ mrFromTable: post });
+    selPost = commonTA.sel.wrap({ mrFromTable: post });
   }
   const consumerTA = mm.tableActions(post, ConsumerTA);
   await testBuildAsync(commonTA, 'configurable-table/wrap/common');
   await testBuildAsync(consumerTA, 'configurable-table/wrap/consumer');
+});
+
+it('configurableTable with WRAP action inside transactions', async () => {
+  // Two tables with the almost same structure.
+  class UserUtil extends User {}
+  const userUtil = mm.table(UserUtil, { virtualTable: true });
+  class CommonTA extends mm.TableActions {
+    insert = mm.insertOne().setInputs();
+    del = mm.deleteOne().by(userUtil.id);
+    upd = mm.updateOne().setInputs().by(userUtil.id);
+    sel = mm.selectRows(userUtil.display_name).by(userUtil.id).orderByAsc(userUtil.display_name);
+  }
+  const commonTA = mm.tableActions(userUtil, CommonTA, { configurableTableName: 'mrFromTable' });
+
+  class ConsumerTA extends mm.TableActions {
+    tx = mm
+      .transact(
+        commonTA.insert.wrap({ mrFromTable: user }),
+        commonTA.del,
+        commonTA.upd,
+        commonTA.sel,
+      )
+      .wrap({ mrFromTable: post });
+  }
+  const consumerTA = mm.tableActions(post, ConsumerTA);
+  await testBuildAsync(commonTA, 'configurable-table/wrap-tx/common');
+  await testBuildAsync(consumerTA, 'configurable-table/wrap-tx/consumer');
 });
--- tests/go/dest/configurable-table/wrap/consumer.go
+++ tests/go/dest/configurable-table/wrap/consumer.go
@@ -21,3 +21,15 @@ func (mrTable *TableTypePost) AddPost(mrQueryable mingru.Queryable, urlName stri
 func (mrTable *TableTypePost) AddUser(mrQueryable mingru.Queryable, urlName string, displayName string, sig *string, age int, followerCount *string) (uint64, error) {
 	return UserUtil.Insert(mrQueryable, User, urlName, displayName, sig, age, followerCount)
 }
+
+func (mrTable *TableTypePost) DelPost(mrQueryable mingru.Queryable, id uint64) error {
+	return UserUtil.Del(mrQueryable, Post, id)
+}
+
+func (mrTable *TableTypePost) SelPost(mrQueryable mingru.Queryable, id uint64) ([]UserUtilTableSelResult, error) {
+	return UserUtil.Sel(mrQueryable, Post, id)
+}
+
+func (mrTable *TableTypePost) UpdPost(mrQueryable mingru.Queryable, id uint64, urlName string, displayName string, sig *string, age int, followerCount *string) error {
+	return UserUtil.Upd(mrQueryable, Post, id, urlName, displayName, sig, age, followerCount)
+}
